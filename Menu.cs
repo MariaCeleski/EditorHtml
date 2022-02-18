7eb95288001be3800e6830e89ab89789e29a4fa7
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-02-18 17:40:49 -0300

    Criando método WriteOptions

--- Menu.cs
+++ Menu.cs
@@ -11,9 +11,13 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
 
         }
         public static void DrawScreen()
+
         {
             Console.Write("+");
             for (int i = 0; i <= 30; i++)
@@ -40,6 +44,26 @@ namespace EditorHtml
 
             Console.Write("+");
             Console.Write("\n");
+
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("===================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.WriteLine("Opção:");
+
 
         }
     }
