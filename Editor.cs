e649ed7978360ddc96642d092f57bc13fa5d402a
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-02-20 11:04:30 -0300

    Criando método Show

---
+++ Editor.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("-----------");
+        }
+
+        public static void Start() { }
+    }
+}
